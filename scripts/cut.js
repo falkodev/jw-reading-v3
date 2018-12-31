063e917341221559fdf3fe0e1ff0f05223c9e7c5
falkodev <tonydbz2002@hotmail.com>
2018-12-31 21:18:12 +0100

    send emai notification after cut

--- scripts/cut.js
+++ scripts/cut.js
@@ -1,8 +1,37 @@
 #!/usr/bin/env node
 
+const cut = require('../src/cut')
+const moment = require('moment')
+const nodemailer = require('nodemailer')
+
 console.log('process entered')
-require('../src/cut')(process.argv[2])
-  .then(fileCreated => {
-    console.log('process ended, file created:' + fileCreated)
+const lang = process.argv[2]
+cut(lang)
+  .then(async fileCreated => {
+    console.log('process ended, file created: %s', fileCreated)
+    const transporter = nodemailer.createTransport({
+      host: 'SSL0.OVH.NET',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'contact@jwreading.com',
+        pass: 'agwxz2002',
+      },
+    })
+    const mailOptions = {
+      from: '"Weekly Cut" <contact@jwreading.com>',
+      to: 'contact@jwreading.com',
+      subject: `Découpage lecture ${lang}`,
+      html: `Découpage lecture ok.<br>Fichier créé: ${fileCreated}<br>Date: ${moment()
+        .locale('fr')
+        .format('LLLL')}`,
+    }
+
+    try {
+      const { messageId } = await transporter.sendMail(mailOptions)
+      console.log('message sent: %s', messageId)
+    } catch (err) {
+      console.log('error while sending mail', err)
+    }
   })
   .catch(err => console.log('error in script cut.js', err))
